d3b91e49276149ffe3491c5932cea26984144338
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-27 14:38:59 +0100

    eightball

---
+++ magiceightball.js
@@ -0,0 +1,34 @@
+var userName = 'Scofield';
+const userQuestion = 'Pick a number';
+var randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+
+userName = ' ' ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+console.log(userQuestion)
+
+switch (randomNumber) {
+  case 0:
+    console.log('It is certain');
+    break;
+  case 1:
+    console.log('It is decidedly so');
+    break;
+  case 2:
+    console.log('Reply hazy try again');
+    break;
+  case 3:
+    console.log('Cannot predict now');
+    break;
+  case 4:
+    console.log('Do not count on it');
+    break;
+  case 5:
+    console.log('My sources say no');
+    break;
+  case 6:
+    console.log('Outlook not so good');
+    break;
+  default:
+    console.log('Signs point to yes');
+}
