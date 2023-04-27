09279e76befc859cb5f513e381e395b60f82d369
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-27 18:10:49 +0100

    raceday

---
+++ raceday.js
@@ -0,0 +1,16 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+const registeredEarly = false;
+const age = 19;
+
+if (registeredEarly && age > 18) {
+  console.log (`Race will begin at 9:30 am and your race number is ${raceNumber}`);
+} 
+else if (!registeredEarly && age > 18) {
+  console.log (`Race will begin at 11:00 am and your race number is ${raceNumber}`);
+}
+else if (age < 18) {
+console.log (`Race will begin at 12:30 am and your race number is ${raceNumber}`);
+}
+else 
+console.log('Go and register at the registration table')
