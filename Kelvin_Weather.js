7e59d246fa4fd6fe7a12c8f52cc80425b63a4759
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-27 04:53:32 +0100

    fahrenheit

--- Kelvin_Weather.js
+++ Kelvin_Weather.js
@@ -1,6 +1,16 @@
-// convert kelvin to celsius 
+// convert from kelvin to celsius 
 
-var kelvin = 0;
-const celsius  = 273 - kelvin; 
+// var kelvin = 0;
+// const celsius  = 273 - kelvin; 
 
-console.log(`The temperature is ${celsius} degrees celsius.`);
+// console.log(`The temperature is ${celsius} degrees celsius.`);
+
+
+//convert from celsius to fahrenheit
+
+var celsius = 0;
+let fahrenheit = celsius * (9 / 5) + 32;
+
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
