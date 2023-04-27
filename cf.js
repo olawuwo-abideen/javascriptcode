68e95d711764d731186446d7a6177e5840379d2d
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-27 05:03:45 +0100

    cf

---
+++ cf.js
@@ -0,0 +1,8 @@
+//convert from celsius to fahrenheit
+
+var celsius = 0;
+let fahrenheit = celsius * (9 / 5) + 32;
+
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
