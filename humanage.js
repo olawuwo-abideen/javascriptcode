3309588c9efb96d0e3f3ee22202761b1be49bedd
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-27 10:28:38 +0100

    human age

---
+++ humanage.js
@@ -0,0 +1,15 @@
+// converting human age to dog age
+const myAge = 20;
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+// changing myage
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+// adding earlyYears and laterYears
+var myAgeInDogYears = earlyYears + laterYears
+var myName = 'Olawuwo Abideen'.toLowerCase();
+
+console.log (`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
---
+++ newton.js
@@ -0,0 +1,12 @@
+// celsius to newton
+
+
+var celsius = 0;
+
+let newton = celsius * (33/100)
+
+newton = Math.floor(newton);
+
+console.log(`The temperature is ${newton} degrees newton.`);
+
+
