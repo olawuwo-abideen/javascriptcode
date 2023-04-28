640ae246fd3f681375ea255646a4c640fdc36d29
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-28 12:15:13 +0100

    rps

---
+++ rps.js
@@ -0,0 +1,32 @@
+var userChoice = prompt("Choose either rock, paper or scissors ?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice ='rock';
+} else if(computerChoice <= 0.67) {
+    computerChoice = 'paper';
+} else {
+	computerChoice = "scissors";
+} console.log("computer: " + computerChoice);
+
+var compare = function(choice1, choice2) {
+if(choice1 === choice2) { 
+return "The result is a tie!";
+}
+else if (choice1 === "rock") {
+if(choice2 === "scissors") {
+return "rock wins";
+}
+else {
+return "paper wins";
+}
+}
+else if (choice2 === "paper"){
+	if(choice1 === "rock"){
+	return "paper wins";
+}
+else {
+return "scissors win";
+}
+}
+}
+compare(userChoice, computerChoice);
